16d1c8f616431c3a9b7f0e3534b5053a06abf0f9
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-09-21 21:07:11 +0600

    new

---
+++ Problem Solving/Apple and Orange.js
@@ -0,0 +1,18 @@
+// Complete the countApplesAndOranges function below.
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let appleCount = 0, orangeCount = 0;
+    for(let i = 0; i < apples.length; i++) {
+        if((a + apples[i]) >= s && (a + apples[i]) <= t) {
+            appleCount++;
+        }
+    }
+
+    for(let i = 0; i < oranges.length; i++) {
+        if((b + oranges[i]) >= s && (b + oranges[i]) <= t) {
+            orangeCount++;
+        }
+    }
+
+    console.log(appleCount);
+    console.log(orangeCount);
+}
